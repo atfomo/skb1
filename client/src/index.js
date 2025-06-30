64e4d908f17e8acc494b1cfefbde9722963fa90b
atfomocom <atfomocom@gmail.com>
2025-06-30 12:49:28 +0530

    update

--- client/src/index.js
+++ client/src/index.js
@@ -7,14 +7,7 @@ import { UserProvider } from './UserContext';
 import { DialogProvider } from './context/DialogContext';
 import { ThemeProvider } from '@mui/material/styles';
 import customTheme from './theme';
-
-// Import your custom axios instance (DO NOT import global axios directly here)
 import axiosInstance from './utils/axiosInstance'; 
-
-// No need to set axios.defaults.baseURL here anymore.
-// No need for global axios interceptors here anymore.
-// The setup is now encapsulated in axiosInstance and called by UserProvider.
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
